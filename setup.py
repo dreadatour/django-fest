57cdc5e165d961d13a84a17be93cd99632b3ec4f
Vladimir Rudnyh <rudnyh@corp.mail.ru>
2012-02-21 12:20:22 +0400

    Build fest javascript files into fest static dir

--- setup.py
+++ setup.py
@@ -7,30 +7,30 @@ from fest import get_version
 packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
 if root_dir:
-    os.chdir(root_dir)
+	os.chdir(root_dir)
 
 for dirpath, dirnames, filenames in os.walk('fest'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[5:] # Strip "fest/" or "fest\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
+	for i, dirname in enumerate(dirnames):
+		if dirname.startswith('.'): del dirnames[i]
+	if '__init__.py' in filenames:
+		pkg = dirpath.replace(os.path.sep, '.')
+		if os.path.altsep:
+			pkg = pkg.replace(os.path.altsep, '.')
+		packages.append(pkg)
+	elif filenames:
+		prefix = dirpath[5:] # Strip "fest/" or "fest\"
+		if prefix == 'static/fest/lib':
+			for f in filenames:
+				data_files.append(os.path.join(prefix, f))
 
 
 setup(name='django-fest',
-    version=get_version(),
-    description='Use fest templates with Django',
-    author='Vladimir Rudnyh',
-    author_email='rudnyh@corp.mail.ru',
-    package_dir={'fest': 'fest'},
-    packages=packages,
-    package_data={'registration': data_files},
+	version=get_version(),
+	description='Use fest templates with Django',
+	author='Vladimir Rudnyh',
+	author_email='rudnyh@corp.mail.ru',
+	package_dir={'fest': 'fest'},
+	packages=packages,
+	package_data={'fest': data_files},
 )
 
